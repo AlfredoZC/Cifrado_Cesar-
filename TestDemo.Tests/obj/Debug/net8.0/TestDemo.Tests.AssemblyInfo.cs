76fcd1f1fbc999cda6729ee75270e73990acc944
AlfredoZC <josealfredo.zambranacruz.az@gmail.com>
2024-05-29 19:17:56 -0400

    Algoritmo Cesar implementado en consola

--- TestDemo.Tests/obj/Debug/net8.0/TestDemo.Tests.AssemblyInfo.cs
+++ TestDemo.Tests/obj/Debug/net8.0/TestDemo.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestDemo.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01fdca21d242d5d57f7dc1c1ec2c3e6b6a795f47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e122fb8dfd0848e8679b3bce8c915d06b7da333e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestDemo.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestDemo.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
